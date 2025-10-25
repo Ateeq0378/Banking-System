22405567619f0205e4a9b248c42ab1d27e448f10
Ateeq Ahmad <ateeq0378@gmail.com>
2025-10-25 20:18:51 +0530

    Finish

---
+++ user/footer.php
@@ -0,0 +1,15 @@
+    <footer id="footer">
+
+        <div class="footer-bottom">
+            <div class="copyright">
+            &copy; Copyright <strong><span>Modern Bank</span></strong>
+            </div>
+            <div class="credits">
+            Designed & Developed by <a href="index.php">Ateeq Ahmad & Shamma Khatoon</a>
+            </div>
+        </div>
+        
+    </footer>
+    
+</body>
+</html>
